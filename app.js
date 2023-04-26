f1b2b2cc40c9340a446a8ae829c171ba98569f1e
mrt39 <mertoktar39@gmail.com>
2023-04-26 21:26:51 +0300

    Add logic for routing with parameters

--- app.js
+++ app.js
@@ -3,6 +3,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+// Load the full lodash build.
+const _ = require('lodash');
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -50,13 +52,29 @@ app.get("/compose", function(req, res){
 
 app.get("/posts/:postTitle", function(req, res){
 
-
-  posts.forEach(function (post) {
-    if (post.title === req.params.postTitle){
+  for (let i = 0; i < posts.length; i++) {
+    //store each of the title we have in our "posts" string in a variable
+    const storedPostTitle = posts[i].title
+    //store what's written as a parameter in URL (what's written as postTitle in /posts/:postTitle) as a variable
+    const requestedPostTitle = req.params.postTitle
+    
+    //check if they match, case INSENSITIVE because we turn both into lowercase (using lodash)
+    if (_.toLower(storedPostTitle) === _.toLower(requestedPostTitle)){
         console.log("Match found")
+        //using break because if match found, we want to get out of the loop
+        break
     }
-  })
+    //also check if they match, even if the url is written in kebabcase (another-post instead of Another Post) 
+    else if (_.kebabCase(storedPostTitle) === _.toLower(requestedPostTitle)) {
+        console.log("Match found")
+        //using break because if match found, we want to get out of the loop
+        break
+    }
+    else{
+        console.log("Not a match.")
 
+    }
+  }
   res.send(req.params.postTitle);
 
 });
